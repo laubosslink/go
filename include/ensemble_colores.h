6e9475b17b9ff0a94c2501ee3c72baeb0cb42df1
Yasmine ELHIMDI <yasmine-elhimdi@hotmail.fr>
2014-02-04 22:57:30 +0100

    ajout de ensemble_colore

---
+++ include/ensemble_colores.h
@@ -0,0 +1,38 @@
+/**
+ * ENSICAEN
+ * 6 Boulevard Marechal Juin
+ * F-14050 Caen Cedex
+ * 
+ * This file is owned by ENSICAEN students.
+ * No portion of this document may be reproduced, copied
+ * or revised without written premission of the authors
+ */
+ 
+/**
+ * @author ELHIMDI Yasmine <elhimdi@ecole.ensicaen.fr>
+ * @author PARMENTIER Laurent <parmentier@ecole.ensicaen.fr, laubosslink@society-lbl.com>
+ * @version 1.1 
+ * @date 03-02-2013
+ * 
+ * @todo remplacer cell par Cell (avec majuscule)
+ */
+ 
+/** 
+ * @file ensemble_colores.h
+ * 
+ * structures Ensemble_colores permettant la manipulation d'un ensemble de position et d'une couleur
+ */
+
+#include <couleur.h>
+#include <positions.h>
+
+typedef struct Ensemble_colores Ensemble_colores;
+ 
+typedef struct Ensemble_colores{
+	Couleur c;
+	Positions p;
+};
+
+
+
+	
---
+++ include/libertes.h
@@ -0,0 +1,27 @@
+/**
+ * ENSICAEN
+ * 6 Boulevard Marechal Juin
+ * F-14050 Caen Cedex
+ * 
+ * This file is owned by ENSICAEN students.
+ * No portion of this document may be reproduced, copied
+ * or revised without written premission of the authors
+ */
+ 
+/**
+ * @author ELHIMDI Yasmine <elhimdi@ecole.ensicaen.fr>
+ * @author PARMENTIER Laurent <parmentier@ecole.ensicaen.fr, laubosslink@society-lbl$
+ * @version 1.0 
+ * @date 03-02-2013
+ */
+ 
+/** 
+ * @file libertes.h
+ * 
+ * L'ensemble de libertes d'un pion (implémentation de Liste)
+ */
+
+#include <ensemble.h>
+
+typedef Ensemble Libertes;
+
