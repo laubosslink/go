65fb789aad2d36b6e9f96913382663ddb2be7727
Yasmine ELHIMDI <yasmine-elhimdi@hotmail.fr>
2014-02-09 23:45:55 +0100

    Modification premier .c Check si c'est bon

--- lib/ensemble/src/ensemble_positions.c
+++ lib/ensemble/src/ensemble_positions.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ensemble.h>
 #include <ensemble_positions.h>
 #include <position.h>
 
@@ -11,77 +12,27 @@ int vide_ensemble_positions(Ensemble_Positions* E){
 	return E->tete == NULL;
 }
 
-Cell* tete(Ensemble_Positions* E){
+Cell* tete_ensemble_positions(Ensemble_Positions* E){
 	return E->tete;
 }
 
-Cell* courant(Ensemble_Positions* E){
+Cell* courant_ensemble_positions(Ensemble_Positions* E){
 	return E->courant;
 }
 
-int suivant(Ensemble_Positions* E){
+int suivant_ensemble_positions(Ensemble_Positions* E){
 	return courant(E)->suivant != NULL;
 }
 
 
-int appartient(Ensemble_Positions* E, Position* element){
-	if(vide(E))
-		return 0;
-		
-	E->courant=E->tete;
-	
-	while(suivant(E) && E->courant->contenu != element ){
-		E->courant = E->courant->suivant;
-	}
-	
-	if( !suivant(E) && E->courant->contenu != element)
-		return 0;
-	
-	return 1;
+int appartient_ensemble_positions(Ensemble_Positions* E, Position* element){
+	return appartient(E,(Position*) element);
 }
 
-Ensemble_Positions* detruire(Ensemble_Positions* E, Position* element){
-	Cell* cellule;
+Ensemble_Positions* detruire_ensemble_positions(Ensemble_Positions* E, Position* element){
+	return detruire(E,(Position*)element);
+}
 	
-	if(!appartient(E,element))
-		return E;
-	
-	cellule = (Cell*) malloc(sizeof(Cell));
-	E->courant=E->tete;
-	
-	while( suivant(E) && E->courant->suivant->contenu != element){
-		E->courant = E->courant->suivant;
-	}
-	
-	if(!suivant(E) && E->courant->contenu != element)
-		return E;
-	
-	cellule = E->courant->suivant;
-	E->courant->suivant = cellule->suivant;
-	
-	free(cellule);
-	
-	return E;
-	
-} 
-	
-void ajouterElement(Ensemble_Positions* E, Position* element){
-	Cell* cellule;
-	
-	cellule=(Cell*)malloc(sizeof(Cell));
-
-	cellule->contenu=element;
-	
-	if(vide(E)){
-		E->tete=cellule;
-	} else {
-		E->courant=E->tete;
-		
-		while(suivant(E) ) {	
-			E->courant=E->courant->suivant;
-			}
-		
-		E->courant->suivant=cellule;
-		cellule->suivant=NULL;
-	}
+void ajouterPosition(Ensemble_Positions* E, Position* element){
+	ajouterElement(E,(Position*)element);
 }
