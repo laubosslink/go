df531abb6f8a9bc60702966fdc0202eb754882fc
Yasmine ELHIMDI <yasmine-elhimdi@hotmail.fr>
2014-02-09 22:44:17 +0100

    Voici les .h deja

---
+++ include/Territoire.h
@@ -0,0 +1,27 @@
+/**
+ * ENSICAEN
+ * 6 Boulevard Marechal Juin
+ * F-14050 Caen Cedex
+ * 
+ * This file is owned by ENSICAEN students.
+ * No portion of this document may be reproduced, copied
+ * or revised without written premission of the authors
+ */
+ 
+/**
+ * @author ELHIMDI Yasmine <elhimdi@ecole.ensicaen.fr>
+ * @author PARMENTIER Laurent <parmentier@ecole.ensicaen.fr, laubosslink@society-lb$
+ * @version 1.0 
+ * @date 03-02-2013
+ */
+ 
+/** 
+ * @file Territoire.h
+ * /@todo revoir la doc 
+ * Une portion de plateau encadré par une ou plusieurs chaines de pions en tenant c$
+ */
+ 
+#include <ensemble_positions.h>
+
+typedef Ensemble_Positions Territoire;
+
