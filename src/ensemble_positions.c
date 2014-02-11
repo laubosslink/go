b20b896756b6324ae3f57589d6041f46069fee03
Yasmine ELHIMDI <yasmine-elhimdi@hotmail.fr>
2014-02-11 20:58:26 +0100

    check fonction appartient prb resolu + test fait

--- src/ensemble_positions.c
+++ src/ensemble_positions.c
@@ -26,15 +26,20 @@ int ensemble_positions_suivant(Ensemble_Positions* E){
 
 int ensemble_positions_appartient(Ensemble_Positions* E, Position* element){
 	E->courant = ensemble_positions_tete(E);
-	while(ensemble_positions_suivant(E) && E->courant->contenu->x != element->x && E->courant->contenu->y != element->y){
+	
+	while(ensemble_positions_suivant(E))
+	{
+		if(((Position*)E->courant->contenu)->x == element->x && ((Position*)E->courant->contenu)->y == element->y )
+					return 1;
 		E->courant = E->courant->suivant;
 	}
-	if(E->courant->suivant == NULL && E->courant->contenu->x != element->x && E->courant->contenu->y != element->y) return 0;
-	return 1;
+	if(((Position*)E->courant->contenu)->x == element->x && ((Position*)E->courant->contenu)->y == element->y) return 1;
+	return 0;
 }
+	
 
-Ensemble_Positions* ensemble_positions_detruire(Ensemble_Positions* E, Position* element){
-	return ensemble_detruire(E,(Position*)element);
+Ensemble_Positions* ensemble_positions_enlever(Ensemble_Positions* E, Position* element){
+	return ensemble_enlever(E,(Position*)element);
 }
 	
 void ensemble_positions_ajouter(Ensemble_Positions* E, Position* element){
